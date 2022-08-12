6e04a8daeeed581badba94d2b8335a89154119eb
Chuazz <94950483+Chuazz@users.noreply.github.com>
2022-08-12 08:33:14 +0700

    Add files via upload

---
+++ assets/js/module/song_list.js
@@ -0,0 +1,66 @@
+const songs = [
+    {
+        name: "Hai Mươi Hai 22",
+        singer: "Amee, Hứa Kim Tuyền",
+        path: "http://docs.google.com/uc?export=open&id=1DA1MtJJ81Q6ZmywsUgfRzmedRRcCsDQQ",
+        image: "./assets/img/HaiMuoiHai.jpg"
+    },
+    {
+        name:  "Tâm Sự Tuổi 30",
+        singer: "Trịnh Thanh Bình",
+        path: "http://docs.google.com/uc?export=open&id=1kiKHHFDOozdYpoLGbHrDhfBioXfUc54C",
+        image: "./assets/img/TamSuTuoi30.jpg"
+    },
+    {
+        name: "Từng Thương",
+        singer: "Phan Duy Anh",
+        path: "http://docs.google.com/uc?export=open&id=1GFNMfwq5Op68CjCEAHAioiuuKwbSdZgA",
+        image: "./assets/img/TungThuong.jpg"
+    },
+    {
+        name: "Yêu Đương Khó Quá Thì Về Khóc Với Anh",
+        singer: "Erik",
+        path: "http://docs.google.com/uc?export=open&id=1NkDc0va1uTMCXXpWpo8E46m34URh2_Sm",
+        image: "./assets/img/YeuDuongkhoQua.jpg"
+    },
+    {
+        name: "3107",
+        singer: "Nâu, Dương, W/N",
+        path: "http://docs.google.com/uc?export=open&id=1pkoDsQRpO7IUm4qt6w2-PqDmmeybwQer",
+        image: "./assets/img/3107.jpg", 
+    },
+    {
+        name: "Tình Yêu Màu Nắng",
+        singer: "Đoàn Thúy Trang, Big Daddy",
+        path: "http://docs.google.com/uc?export=open&id=18NIFJVwYrE4IdMLnrYij1XDP8aVVx5km",
+        image: "./assets/img/TinhYeuMauNang.jpg",
+    },
+    {
+        name: "Thu Cuối",
+        singer: "Mr T x Yanbi x Hằng Bingboong",
+        path: "http://docs.google.com/uc?export=open&id=1-jTTjlMhWnsJ37VvqStWkoAD9gM--Mfd",
+        image: "./assets/img/ThuCuoi.jpg",
+    },
+    {
+        name: "Có Như Không Có",
+        singer: "Hiền Hồ",
+        path: "http://docs.google.com/uc?export=open&id=1yLfECWCK0q8W___0aYsd0mbJy2etiE1D",
+        image: "./assets/img/CoNhuKhongCo.jpg",
+    },
+    {
+        name: "Cô Gái Vànd",
+        singer: "HuyR x Tùng Viu x Quang Đăng",
+        path: "http://docs.google.com/uc?export=open&id=1q3hCp7YbvPa9YsplCqXUvgK2nmxAE4m6",
+        image: "./assets/img/CoGaiVang.jpg",
+    },
+    {
+        name: "Hạ Sang",
+        singer: "Đào Duy Quý, lenguyenhuukhanh",
+        path: "http://docs.google.com/uc?export=open&id=1FRtURfMGzgQ3xvnhiLaq3yLl4WkTVc6S",
+        image: "./assets/img/HaSang.jpg",
+    },
+];
+
+export{
+    songs,
+}
